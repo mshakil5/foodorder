c138019fe758b9024dae5df929692bd241f55f03
shakil <kazimuhammadullah@gmail.com>
2024-02-02 17:54:20 +0600

    food

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,6 +8,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 		{{-- <link rel="stylesheet" href="css/bootstrap.min.css"/>
 		<script src="js/jquery2.js"></script>
+		Shambleskorner@123!
 		<script src="js/bootstrap.min.js"></script> --}}
 		<!-- <script src="js/main.js"></script> -->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
