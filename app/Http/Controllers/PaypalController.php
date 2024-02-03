e77f7c078237a22ac7431912f3eccf31c937e46b
shakil <kazimuhammadullah@gmail.com>
2024-02-03 13:07:17 +0600

    food

--- app/Http/Controllers/PaypalController.php
+++ app/Http/Controllers/PaypalController.php
@@ -251,10 +251,19 @@ class PaypalController extends Controller
                     $contactmail = $request->email;
                     $ccEmails = "kmushakil93@gmail.com";
                     $msg = "Thank you for your order.";
-                    
+                    $orderDtls = OrderDetail::with('orderadditionalitem')->where('order_id', $order->id)->get();
+                
                     if (isset($msg)) {
                         $array['name'] = $request->name;
                         $array['email'] = $request->email;
+                        $array['phone'] = $request->phone;
+                        $array['invoiceno'] = $order->invoiceno;
+                        $array['payment_type'] = $order->payment_type;
+                        $array['delivery_type'] = $order->delivery_type;
+                        $array['collection_time'] = $order->collection_time;
+                        $array['orderDtls'] = $orderDtls;
+                        $array['date'] = $order->date;
+                        $array['net_amount'] = $order->net_amount;
                         $array['subject'] = "Order Booking Confirmation";
                         $array['message'] = $msg;
                         $array['contactmail'] = $contactmail;
