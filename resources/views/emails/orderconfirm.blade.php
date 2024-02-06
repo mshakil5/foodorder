6c1bf003b5c12c2b60086ef6b14c8cff54dfc18a
shakil <kazimuhammadullah@gmail.com>
2024-02-06 18:09:43 +0600

    food

--- resources/views/emails/orderconfirm.blade.php
+++ resources/views/emails/orderconfirm.blade.php
@@ -115,7 +115,7 @@
         <div class="row">
             <div class="col-md-12">
                 
-                <h3 class="panel-title"><strong>Order summary:</strong></h3>
+                <h3 class="panel-title" style="color: #74787E"><strong>Order Summary:</strong></h3>
             </div>
             <div class="col-md-12">
 
